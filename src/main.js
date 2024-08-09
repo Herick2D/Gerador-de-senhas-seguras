faa794b776b3ca0ac993dd9a2ccc981e354a46a5
Herick2D <contato.herickmoreira@gmail.com>
2024-08-09 16:52:50 -0300

    feat: add copy click in logic

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 import { nanoid } from 'nanoid';
+import copy from 'clipboard-copy';
 import './style.css';
 
 
 const passwordBtnEl = document.querySelector('button');
 const displayPasswordEl = document.querySelector('h2');
 
+let randomPassword = '';
+
 passwordBtnEl.addEventListener('click', () => {
-    const randomPassword = nanoid();
+    randomPassword = nanoid();
     displayPasswordEl.innerHTML = randomPassword;
+});
+
+displayPasswordEl.addEventListener('click', () => {
+    copy(randomPassword);
 });
